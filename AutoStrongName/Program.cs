c4fe9c6ba95adc499a31db9a77b10a77c9615006
picrap <pascal@craponne.fr>
2015-04-15 12:02:29 +0200

    Test to parse VS projects
    And load references assemblies

---
+++ AutoStrongName/Program.cs
@@ -0,0 +1,27 @@
+﻿#region AutoStrongName
+// AutoStrongName
+// An automatic tool to presign unsigned dependencies
+// https://github.com/picrap/AutoStrongName
+#endregion
+
+namespace AutoStrongName
+{
+    using System.Linq;
+    using Build;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var project = new Project(@"..\..\AutoStrongName.csproj");
+            var references = project.GetReferences().ToArray();
+            foreach (var r in references)
+            {
+                var a = r.Resolve();
+                if (a.GlobalAssemblyCache)
+                    continue;
+                var t = a.GetName().GetPublicKey();
+            }
+        }
+    }
+}
