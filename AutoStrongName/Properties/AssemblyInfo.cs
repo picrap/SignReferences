cb9b7d1c0f6c976fb0649cd00231cac963a716ed
picrap <pascal@craponne.fr>
2015-04-16 17:07:56 +0200

    A good nupkg
    But still non fully working

--- AutoStrongName/Properties/AssemblyInfo.cs
+++ AutoStrongName/Properties/AssemblyInfo.cs
@@ -6,21 +6,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// --- Product
-
-[assembly: AssemblyCompany("craponne.fr ;)")]
-[assembly: AssemblyProduct("AutoStrongName")]
-
-[assembly: AssemblyCopyright("Distributed under MIT license http://opensource.org/licenses/MIT")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("0.1")]
-
-// --- Product
-
 [assembly: AssemblyTitle("AutoStrongName")]
-[assembly: AssemblyDescription("Signs unsigned assemblies at build-time. This is useful if your project requires signing and you use third-party unsigned libraries.")]
 
 [assembly: ComVisible(false)]
 
