fec10fc2f3faa5698ce4f7ba157bc9c63625c946
picrap <pascal@craponne.fr>
2015-04-16 15:31:07 +0200

    Almost a good nupkg
    ... But not.

--- AutoStrongName/Properties/AssemblyInfo.cs
+++ AutoStrongName/Properties/AssemblyInfo.cs
@@ -1,36 +1,26 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿#region AutoStrongName
+// An automatic tool to presign unsigned dependencies
+// https://github.com/picrap/AutoStrongName
+#endregion
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Les informations générales relatives à un assembly dépendent de 
-// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
-// associées à un assembly.
-[assembly: AssemblyTitle("AutoStrongName")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+// --- Product
+
+[assembly: AssemblyCompany("craponne.fr ;)")]
 [assembly: AssemblyProduct("AutoStrongName")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+
+[assembly: AssemblyCopyright("Distributed under MIT license http://opensource.org/licenses/MIT")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
-// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
-// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: AssemblyVersion("0.1")]
+
+// --- Product
+
+[assembly: AssemblyTitle("AutoStrongName")]
+[assembly: AssemblyDescription("Signs unsigned assemblies at build-time. This is useful if your project requires signing and you use third-party unsigned libraries.")]
+
 [assembly: ComVisible(false)]
 
-// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("ddeb841e-6065-4da2-981c-e59e3d6e3846")]
-
-// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
-//
-//      Version principale
-//      Version secondaire 
-//      Numéro de build
-//      Révision
-//
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
-// en utilisant '*', comme indiqué ci-dessous :
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
